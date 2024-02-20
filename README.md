318337e7232fefa55c8b8da2bf8b65f8b8b284c2
Claudia <clau.v.perlado@gmail.com>
2024-02-20 16:25:31 +0100

    segundo index

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Entrega-1-iot
 Repositorio para la entrega de la primera práctica en la asignatura de Desarrollo de aplicaciones IOT
+
+• Miembros del equipo: Claudia Viñals Perlado
+• Explicación de los pasos seguidos
+    1- Elegir las versiones de elasticsearch y filebeat. 
+    2- Crear el docker-compose con elasticsearch, filebeats y python.
+    3- Ejecutar docker-compose y descargar las imagenes
+    4- Programar el código para generar los datos 
+• Instrucciones de uso
+    1. Instalación del docker compose yendo a la carpeta donde se encuentra el docker-compose.yml para ello usar este comando:
+        docker-compose up -d
+• Posibles vías de mejora
+• Problemas / Retos encontrados
+• Alternativas posibles
--- generador_datos.py
+++ generador_datos.py
@@ -4,7 +4,7 @@ import threading
 import time
 import os
 
-def generate_dummy_data():
+def generate_casa_data():
     # Generar datos ficticios para una casa
     data = {
         "timestamp": int(time.time()),
@@ -14,6 +14,18 @@ def generate_dummy_data():
             "size_sqft": random.randint(500, 5000),
             "year_built": random.randint(1900, 2022),
             "price_usd": random.randint(100000, 1000000)
+        }
+    }
+    return json.dumps(data)
+
+def generate_ciudad_data():
+    # Generar datos ficticios para una ciudad
+    data = {
+        "timestamp": int(time.time()),
+        "city": {
+            "name": random.choice(ciudades),
+            "population": random.randint(poblacion_minima, poblacion_maxima),
+            "country": "España"
         }
     }
     return json.dumps(data)
@@ -28,11 +40,196 @@ def generate_and_write_data():
     if not os.path.exists(data_folder):
         os.makedirs(data_folder)
         
-    # Escribir los datos en el archivo dummy_data.json
-    with open(os.path.join(data_folder, "dummy_data.json"), "a") as file:
-        datos = generate_dummy_data()
+    # Escribir los datos en el archivo casas.json
+    with open(os.path.join(data_folder, "casas_data.json"), "a") as file:
+        datos = generate_casa_data() +"\n"
         print(datos)
         file.write(datos)
+    # Escribir los datos en el archivo city_data.json
+    with open(os.path.join(data_folder, "ciudades_data.json"), "a") as file:
+        data = generate_ciudad_data()
+        print(data)
+        file.write(data + '\n')
+
+
 
 espera = 5
+ciudades = [
+    "Madrid",
+    "Barcelona",
+    "Valencia",
+    "Sevilla",
+    "Zaragoza",
+    "Málaga",
+    "Murcia",
+    "Palma de Mallorca",
+    "Las Palmas de Gran Canaria",
+    "Bilbao",
+    "Alicante",
+    "Córdoba",
+    "Valladolid",
+    "Vigo",
+    "Gijón",
+    "L'Hospitalet de Llobregat",
+    "Vitoria-Gasteiz",
+    "La Coruña",
+    "Granada",
+    "Elche",
+    "Oviedo",
+    "Santa Cruz de Tenerife",
+    "Pamplona",
+    "Cartagena",
+    "Sabadell",
+    "Jerez de la Frontera",
+    "Móstoles",
+    "Alcalá de Henares",
+    "Fuenlabrada",
+    "Almería",
+    "Terrassa",
+    "Badalona",
+    "Getafe",
+    "Donostia-San Sebastián",
+    "Leganes",
+    "Santander",
+    "Burgos",
+    "Salamanca",
+    "Alcorcón",
+    "Albacete",
+    "Castellón de la Plana",
+    "Logroño",
+    "Badajoz",
+    "Huelva",
+    "Marbella",
+    "León",
+    "Tarragona",
+    "Cádiz",
+    "Lleida",
+    "Jaén",
+    "Ourense",
+    "Mataró",
+    "Algeciras",
+    "Alcobendas",
+    "Cáceres",
+    "Reus",
+    "Telde",
+    "Torrejón de Ardoz",
+    "Barakaldo",
+    "Parla",
+    "Santiago de Compostela",
+    "San Fernando",
+    "Lugo",
+    "Girona",
+    "Coslada",
+    "Talavera de la Reina",
+    "Cornellà de Llobregat",
+    "El Puerto de Santa María",
+    "Melilla",
+    "Orihuela",
+    "Pontevedra",
+    "Pozuelo de Alarcón",
+    "Roquetas de Mar",
+    "Chiclana de la Frontera",
+    "Torrent",
+    "San Sebastián de los Reyes",
+    "Rubí",
+    "Benidorm",
+    "Fuengirola",
+    "Ciudad Real",
+    "Las Rozas de Madrid",
+    "Benalmádena",
+    "Sanlúcar de Barrameda",
+    "Vélez-Málaga",
+    "Torrevieja",
+    "Zamora",
+    "Rivas-Vaciamadrid",
+    "Vilanova i la Geltrú",
+    "Torrelavega",
+    "Línea de la Concepción",
+    "Gandia",
+    "Manresa",
+    "San Vicente del Raspeig",
+    "Santa Lucía de Tirajana",
+    "Mijas",
+    "Majadahonda",
+    "Sagunto",
+    "Paterna",
+    "San Sebastián de La Gomera",
+    "Viladecans",
+    "Torremolinos",
+    "Estepona",
+    "Rubí",
+    "Molina de Segura",
+    "Torrelodones",
+    "Elda",
+    "Ponferrada",
+    "Alcobendas",
+    "Arrecife",
+    "Cuenca",
+    "Valdemoro",
+    "Pinto",
+    "Sant Cugat del Vallès",
+    "Palencia",
+    "Alcantarilla",
+    "Linares",
+    "San Bartolomé de Tirajana",
+    "Roquetas de Mar",
+    "Coslada",
+    "Tudela",
+    "El Prat de Llobregat",
+    "San Juan de Alicante",
+    "Vinaròs",
+    "Coslada",
+    "Segovia",
+    "Cártama",
+    "Avilés",
+    "Aranda de Duero",
+    "Zarautz",
+    "Vic",
+    "Oliva",
+    "Sestao",
+    "Irun",
+    "Ávila",
+    "Parets del Vallès",
+    "Paterna",
+    "Alzira",
+    "Benicarló",
+    "Carballo",
+    "Vila-real",
+    "Sant Adrià de Besòs",
+    "Villanueva y Geltrú",
+    "Narón",
+    "Camargo",
+    "Puerto del Rosario",
+    "El Ejido",
+    "Arona",
+    "Manacor",
+    "Pineda de Mar",
+    "Xàtiva",
+    "Mogán",
+    "Maó",
+    "Los Realejos",
+    "Vélez-Málaga",
+    "Villaquilambre",
+    "El Masnou",
+    "La Orotava",
+    "Ripollet",
+    "Santurtzi",
+    "Mazarrón",
+    "Begur",
+    "Benavente",
+    "Leganés",
+    "Castro-Urdiales",
+    "Caldes de Montbui",
+    "Tàrrega",
+    "La Oliva",
+    "Mijas",
+    "Sant Andreu de la Barca",
+    "Portugalete",
+    "Molina de Aragón",
+    "Adra"
+  ]
+
+poblacion_minima = 100000
+poblacion_maxima = 3000000
+
 generate_and_write_data()
